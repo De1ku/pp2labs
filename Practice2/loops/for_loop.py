46eb00003aa344738f7475798982a36503312b96
De1ku <dekusikdeik@gmail.com>
2026-02-03 23:05:33 +0500

    for examples added

---
+++ Practice2/loops/for_loop.py
@@ -0,0 +1,27 @@
+colors = ["red", "green", "blue"]
+for c in colors:
+    print(c)
+
+
+for i in range(5):
+    print(i)
+
+
+
+nums = [1, 2, 3, 4]
+s = 0
+for x in nums: #сумма эл-ов
+    s += x
+print(s)
+
+
+
+word = "hello"
+for ch in word:
+    print(ch)
+
+
+
+scores = {"Ann": 5, "Bob": 3}
+for name, score in scores.items():
+    print(name, score)
